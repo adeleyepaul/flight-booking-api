4e33db1bfdea4cd8b41bdd0091331a0033530915
adeleyepaul <adeleyepaul>
2022-11-10 09:56:53 +0100

    fourth commit

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,52 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+// exports.example = (req, res) => {
+//     console.log("example")
+//     res.send("Flight example")
+// }
+
+const {v4:uuidv4} = require('uuid')
+
+const flight = []
+
+exports.getBook = (req, res) => {
+    res.send(flight); 
 }
 
 
+exports.flightBook = (req, res) => {
+    const booking = req.body;
+
+    flight.push({...booking, id: uuidv4() })
+
+    res.send(`A new flight booking with ${booking.title} has been added`);
+
+}
+
+exports.getUserBook = (req,res) => {
+    const {id} = req.params;
+
+    const findBooking = flight.find((booking) => booking.id == id)
+
+    res.send(findBooking);
+}
+
+exports.deleteBook = (req, res) => {
+    const { id } = req.params;
+
+    const ticket = flight.filter((booking) => booking.id != id);
+
+    res.send(ticket);
+}
+
+exports.updateBook = (req, res) => {
+    const { id } = req.params;
+    const {title, time, price, date} = req.body;
+
+    const UpDate = flight.find((booking) => booking.id === id);
+
+    if(title) UpDate.title = title;
+    if(time) UpDate.time = time;
+    if(price) UpDate.price = price;
+    if(date) UpDate.date = date;
+
+    res.send(`Flight booking with id ${id} has been updated`);
+}
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -1,55 +1,17 @@
 const express = require('express')
-const {v4:uuidv4} = require('uuid')
+
+const controller = require('../controllers/flightController')
 
 const router = express.Router();
 
-const flight = []
+router.get('/', controller.getBook);
 
-router.get('/', (req, res) => {
+router.post('/', controller.flightBook);
 
-    res.send(flight);
-})
+router.get('/:id', controller.getUserBook)
 
-router.post('/', (req, res) => {
-    const booking = req.body;
+router.delete('/:id', controller.deleteBook);
 
-    flight.push({...booking, id: uuidv4() })
-
-    res.send(`A new flight booking with ${booking.title} has been added`);
-
-    // console.log("post reached")
-
-    // res.send("post reached")
-})
-
-router.get('/:id', (req,res) => {
-    const {id} = req.params;
-
-    const findBooking = flight.find((booking) => booking.id == id)
-
-    res.send(findBooking);
-})
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const ticket = flight.filter((booking) => booking.id != id);
-
-    res.send(ticket);
-})
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const {title, time, price, date} = req.body;
-
-    const UpDate = flight.find((booking) => booking.id === id);
-
-    if(title) UpDate.title = title;
-    if(time) UpDate.time = time;
-    if(price) UpDate.price = price;
-    if(date) UpDate.date = date;
-
-    res.send(`Flight booking with id ${id} has been updated`);
-})
+router.patch('/:id', controller.updateBook);
 
 module.exports = router;
